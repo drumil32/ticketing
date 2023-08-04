6c7a933a39bcd3666482a334d5b11fd03bb01fba
Drumil_Akhenia <akheniad@gmail.com>
2023-08-05 01:38:35 +0530

    refact:auth

--- auth/src/routes/sign-out.ts
+++ auth/src/routes/sign-out.ts
@@ -1,10 +1,10 @@
 
-import {Router} from 'express';
+import { Router } from 'express';
 import { Session } from 'express-session';
 
 const router = Router();
 
-router.post('/api/users/sign-out',(req,res) => {
+router.post('/api/users/sign-out', (req, res) => {
     req.session = null as unknown as Session;
     res.send({});
 });
